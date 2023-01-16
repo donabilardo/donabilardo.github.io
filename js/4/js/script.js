23997a3a8c401e0057957e86b47c32fc12a5645d
Don Abilardo <don.abilardo@gmail.com>
2023-01-16 19:59:41 +0500

    JS Junior

---
+++ js/4/js/script.js
@@ -0,0 +1,31 @@
+let a = document.getElementsByClassName("cl1")[0]
+
+a.classList.add("cl2");
+
+lastReg = new Date()
+
+let users = {
+    name: 'admin',
+    pass: '12345',
+    last: lastReg
+}
+
+
+let mass1 = ['one', 'two', 'three']
+
+ const myFunct = new Function();
+
+ let x = String(1115455)
+
+ x = Number(x)
+
+x = Math.random() * 100
+
+ console.log(x);
+ console.log(typeof(x));
+
+ x = Math.ceil(x)
+ console.log(x);
+
+
+ console.log(x.length);
